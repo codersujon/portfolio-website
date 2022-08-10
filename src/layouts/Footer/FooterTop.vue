5a8ac55a8658c3991eca7b42ae2d65f8bb0861a7
Md. Sujan Ahmed <shobuj2sujon@gmail.com>
2022-08-11 04:46:25 +0600

    grid apply to footer

--- src/layouts/Footer/FooterTop.vue
+++ src/layouts/Footer/FooterTop.vue
@@ -1,17 +1,17 @@
 <template>
     <section class="footerTop bg-slate-900 text-center">
-        <div class="container mx-auto footerTop__content py-28 px-4">
+        <div class="footerTop__content container mx-auto py-28 px-4">
             <h2 class="text-white font-bold pb-12 text-4xl sm:text-5xl">Let's work together</h2>
-            <div class="footerTop__socials text-white flex flex-col md:flex-row justify-between">
-                <div class="footerTop__widget text-left my-2 sm:my-4">
+            <div class="footerTop__socials text-white grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+                <div class="footerTop__widget text-left my-2 sm:my-4 lg:m-auto">
                     <span class="capitalize text-slate-100 inline-block mb-1">call</span>
                     <p class="text-2xl lg:text-3xl  font-medium">+8801970 366 446</p>
                 </div>
-                <div class="footerTop__widget text-left my-2 sm:my-4">
+                <div class="footerTop__widget text-left my-2 sm:my-4 lg:m-auto">
                     <span class="capitalize text-slate-100 inline-block mb-1">email:</span>
                     <p class="text-2xl lg:text-3xl  font-medium">csesujon155@gmail.com</p>
                 </div>
-                <div class="footerTop__widget text-left my-2 sm:my-4">
+                <div class="footerTop__widget text-left my-2 sm:my-4 lg:m-auto">
                     <span class="capitalize text-slate-100 inline-block mb-1">follow me:</span>
                     <div class="social__icons flex space-x-3">
                         <a href="https://www.facebook.com/CoderSujon/" class="w-10 h-10 leading-10 bg-red-600 text-center rounded-sm hover:bg-white hover:text-red-600 transition duration-200 ease-in" target="_blank">
@@ -31,3 +31,4 @@
 </template>
 
 
+<!-- flex flex-col md:flex-row justify-between -->
