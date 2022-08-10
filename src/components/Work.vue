d972f0074bad2ca0af67357e11699b704f82f06c
Md. Sujan Ahmed <shobuj2sujon@gmail.com>
2022-08-11 05:34:19 +0600

    work half

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -1,10 +1,81 @@
 <template>
-    <section class="work py-28 ">
+    <section class="work py-28">
         <div class="container mx-auto px-4">
             <SectionTitleVue 
-                title="Creative Portfolio" 
-                text="Check out my latest creative works. I have done a lot of work for my local and international clients. Here is a shortlist of my newly designed websites."
+                title="My Latest Work" 
+                text="Experience without theory is blind, so I have done almost 10+ projects for my local and international clients. Here is a shortlist."
             />
+            <div class="work__content pt-20 grid grid-cols-1">
+                <div class="workItem">
+                    <div class="workItem__img">
+                        <div class="workItem__overlay">
+                            <div class="workItem__overlay__icon">
+                                <h4>PSD to HTML</h4>
+                                <a href="https://positiveworldbd.github.io/Tajam-Template/" target="_blank" rel="noreferrer">
+                                    <i className="fa-solid fa-eye"></i>
+                                </a>
+                                <a href="https://www.youtube.com/watch?v=KItCFl5GgdI" target="_blank" rel="noreferrer">
+                                    <i className="fa-solid fa-play"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="workItem__content">
+                        <h4>PSD to HTML</h4>
+                        <div className="portfolioItem__info-link ">
+                            <a href="https://positiveworldbd.github.io/Tajam-Template/" target="_blank" rel="noreferrer">
+                                <i className="fa-solid fa-link"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="workItem">
+                    <div class="workItem__img">
+                        <div class="workItem__overlay">
+                            <div class="workItem__overlay__icon">
+                                <h4>PSD to HTML</h4>
+                                <a href="https://positiveworldbd.github.io/Tajam-Template/" target="_blank" rel="noreferrer">
+                                    <i className="fa-solid fa-eye"></i>
+                                </a>
+                                <a href="https://www.youtube.com/watch?v=KItCFl5GgdI" target="_blank" rel="noreferrer">
+                                    <i className="fa-solid fa-play"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="workItem__content">
+                        <h4>PSD to HTML</h4>
+                        <div className="portfolioItem__info-link ">
+                            <a href="https://positiveworldbd.github.io/Tajam-Template/" target="_blank" rel="noreferrer">
+                                <i className="fa-solid fa-link"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="workItem">
+                    <div class="workItem__img">
+                        <div class="workItem__overlay">
+                            <div class="workItem__overlay__icon">
+                                <h4>PSD to HTML</h4>
+                                <a href="https://positiveworldbd.github.io/Tajam-Template/" target="_blank" rel="noreferrer">
+                                    <i className="fa-solid fa-eye"></i>
+                                </a>
+                                <a href="https://www.youtube.com/watch?v=KItCFl5GgdI" target="_blank" rel="noreferrer">
+                                    <i className="fa-solid fa-play"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="workItem__content">
+                        <h4>PSD to HTML</h4>
+                        <div className="portfolioItem__info-link ">
+                            <a href="https://positiveworldbd.github.io/Tajam-Template/" target="_blank" rel="noreferrer">
+                                <i className="fa-solid fa-link"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 </template>
@@ -16,7 +87,9 @@ export default {
 
     data() {
         return {
-            
+            itemName: "",
+            livePreview: "",
+
         };
     },
 
@@ -35,5 +108,9 @@ export default {
 </script>
 
 <style scoped>
-
+.workItem__img{
+    background-image: url('/assets/images/portfolio/Project__bg-1.png') no-repeat;
+    background-size: cover;
+    background-position: center center;
+}
 </style>
