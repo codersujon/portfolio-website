2c012b370e798cb7e27bd0df899a278cd53e86c9
Md. Sujan Ahmed <shobuj2sujon@gmail.com>
2022-08-10 19:28:42 +0600

    Error Solved

--- src/layouts/Header/Logo.vue
+++ src/layouts/Header/Logo.vue
@@ -1,11 +1,9 @@
 <template>
     <div class="logo transform hover:scale-110 duration-500">
         <a href="/">
-            <img src="/logo.png" alt="logo">
+            <img src="/logo.png" alt="">
         </a>
     </div>
 </template>
-
-<script>
 
 
