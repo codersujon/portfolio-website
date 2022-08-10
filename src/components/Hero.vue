4ed68013aa38848d8fe5be09651c89325e83d586
Md. Sujan Ahmed <shobuj2sujon@gmail.com>
2022-08-11 04:04:45 +0600

    grid making

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,15 +1,17 @@
 <template>
     <section class="hero py-28">
-        <div class="container mx-auto flex flex-col md:flex-row justify-between 2xl:justify-evenly  items-center px-4">
-            <div class="hero__content space-y-3 mb-20 ">
+        <div class="container mx-auto px-4 grid grid-cols-1 gap-8 lg:grid-cols-2 ">
+            <div class="hero__content space-y-3 mb-20 text-center lg:text-left justify-center">
                 <h3 class="font-thin">Hello! <span class="text-red-400">I'm</span></h3>
                 <h1 class="uppercase text-5xl xl:text-6xl"><span class="text-red-500">Md. Sujan </span>    Ahmed</h1>
                 <h4 class="text-red-400 mb-2">B.Sc Hon's in CSE</h4>
                 <p class="text-slate-500">I’m a highly motivated, ambitious, and creative person who can make <br/> you happy to develop an amazing website.</p>
             </div>
-            <div class="hero__image">
+            <div class="hero__image m-auto ">
                 <img src="/assets/images/hero.png" alt="My-Image">
             </div>
         </div>
     </section>
 </template>-
+
+<!--  flex-col md:flex-row  2xl:justify-evenly  items-center -->
--- src/components/Skill.vue
+++ src/components/Skill.vue
@@ -5,37 +5,37 @@
                 title="My Skill" 
                 text="Now, I have quite a good grasp in HTML, CSS, SASS, Bootstrap, TailwindCSS, JavaScript, jQuery, React JS, Vue JS, Typescript, PHP, MYSQL, Git, and so on."
             />
-            <div class="skill__content pt-20 grid grid-cols-2 gap-8 items-center">
-                <div class="skill__img ">
+            <div class="skill__content pt-20 grid grid-cols-1 lg:grid-cols-2 gap-8 items-center">
+                <div class="skill__img mb-16 m-auto">
                     <img src="/assets/images/skill/skill.png" alt="Skill">
                 </div>
-                <div class="skill__lists grid grid-cols-4 gap-8">
-                    <div class="skill__item mr-8">
-                        <img src="/assets/images/skill/html.png" alt="html">
+                <div class="skill__lists grid grid-cols-4 sm:grid-cols-3 gap-8">
+                    <div class="skill__item m-auto text-center">
+                        <img src="/assets/images/skill/html.png" alt="html" class="m-auto">
                         <h4 class="capitalize text-2xl">html</h4>
                     </div>
-                    <div class="skill__item mr-8">
-                        <img src="/assets/images/skill/css.png" alt="css">
+                    <div class="skill__item m-auto text-center">
+                        <img src="/assets/images/skill/css.png" alt="css" class="m-auto">
                         <h4 class="capitalize text-2xl">css</h4>
                     </div>
-                    <div class="skill__item mr-8">
-                        <img src="/assets/images/skill/sass.png" alt="sass">
+                    <div class="skill__item m-auto text-center">
+                        <img src="/assets/images/skill/sass.png" alt="sass" class="m-auto">
                         <h4 class="capitalize text-2xl">sass</h4>
                     </div>
-                    <div class="skill__item mr-8">
-                        <img src="/assets/images/skill/bootstrap.png" alt="bootstrap">
+                    <div class="skill__item m-auto text-center">
+                        <img src="/assets/images/skill/bootstrap.png" alt="bootstrap" class="m-auto">
                         <h4 class="capitalize text-2xl">bootstrap</h4>
                     </div>
-                    <div class="skill__item">
-                        <img src="/assets/images/skill/tailwind.png" alt="tailwind">
+                    <div class="skill__item text-center">
+                        <img src="/assets/images/skill/tailwind.png" alt="tailwind" class="m-auto">
                         <h4 class="capitalize text-2xl">tailwind</h4>
                     </div>
-                    <div class="skill__item">
-                        <img src="/assets/images/skill/js.png" alt="js">
+                    <div class="skill__item m-auto text-center">
+                        <img src="/assets/images/skill/js.png" alt="js" class="m-auto">
                         <h4 class="capitalize text-2xl">js</h4>
                     </div>
-                    <div class="skill__item">
-                        <img src="/assets/images/skill/jquery.png" alt="jquery">
+                    <div class="skill__item m-auto text-center">
+                        <img src="/assets/images/skill/jquery.png" alt="jquery" class="m-auto">
                         <h4 class="capitalize text-2xl">jquery</h4>
                     </div>
                 </div>
