a51f67141457e5b01739fe125ebeea1f293e79af
Md. Sujan Ahmed <shobuj2sujon@gmail.com>
2022-08-11 03:28:53 +0600

    skill done

--- src/components/Skill.vue
+++ src/components/Skill.vue
@@ -1,10 +1,45 @@
 <template>
-    <section class="achiemement py-28">
+    <section class="skill py-28">
         <div class="container mx-auto px-4">
             <SectionTitleVue 
                 title="My Skill" 
                 text="Now, I have quite a good grasp in HTML, CSS, SASS, Bootstrap, TailwindCSS, JavaScript, jQuery, React JS, Vue JS, Typescript, PHP, MYSQL, Git, and so on."
             />
+            <div class="skill__content pt-20 grid grid-cols-2 gap-8 items-center">
+                <div class="skill__img ">
+                    <img src="/assets/images/skill/skill.png" alt="Skill">
+                </div>
+                <div class="skill__lists grid grid-cols-4 gap-8">
+                    <div class="skill__item mr-8">
+                        <img src="/assets/images/skill/html.png" alt="html">
+                        <h4 class="capitalize text-2xl">html</h4>
+                    </div>
+                    <div class="skill__item mr-8">
+                        <img src="/assets/images/skill/css.png" alt="css">
+                        <h4 class="capitalize text-2xl">css</h4>
+                    </div>
+                    <div class="skill__item mr-8">
+                        <img src="/assets/images/skill/sass.png" alt="sass">
+                        <h4 class="capitalize text-2xl">sass</h4>
+                    </div>
+                    <div class="skill__item mr-8">
+                        <img src="/assets/images/skill/bootstrap.png" alt="bootstrap">
+                        <h4 class="capitalize text-2xl">bootstrap</h4>
+                    </div>
+                    <div class="skill__item">
+                        <img src="/assets/images/skill/tailwind.png" alt="tailwind">
+                        <h4 class="capitalize text-2xl">tailwind</h4>
+                    </div>
+                    <div class="skill__item">
+                        <img src="/assets/images/skill/js.png" alt="js">
+                        <h4 class="capitalize text-2xl">js</h4>
+                    </div>
+                    <div class="skill__item">
+                        <img src="/assets/images/skill/jquery.png" alt="jquery">
+                        <h4 class="capitalize text-2xl">jquery</h4>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 </template>
