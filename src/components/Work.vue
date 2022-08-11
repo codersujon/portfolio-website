9f197378e6859f2c07c0d485fe3dd45123a71897
Md. Sujan Ahmed <shobuj2sujon@gmail.com>
2022-08-11 09:07:07 +0600

    work extend

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -6,7 +6,7 @@
                 text="Experience without theory is blind, so I have done almost 10+ projects for my local and international clients. Here is a shortlist."
             />
             
-            <div class="work__content pt-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
+            <div class="work__content pt-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-10">
                 <div class="workItem bg-[url('/assets/images/portfolio/Project__bg-1.png')] w-full relative bg-cover">
                     <div class="workItem__img w-96 h-96 cursor-pointer transform hover:scale-110 duration-500">
                     </div>
@@ -45,6 +45,54 @@
                     </div>
                 </div>
                 <div class="workItem bg-[url('/assets/images/portfolio/Project__bg-4.png')] w-full relative bg-cover">
+                    <div class="workItem__img w-96 h-96 cursor-pointer transform hover:scale-110 duration-500">
+                    </div>
+                    <div class="workItem__content text-white bg-slate-800 flex justify-between items-center p-5">
+                        <h5>PSD to HTML</h5>
+                        <div className="portfolioItem__info-link w-10 h-10 leading-10 bg-red-600 text-center border-2 border-red-800 rounded-full transform hover:scale-110 duration-300">
+                            <a href="https://positiveworldbd.github.io/Tajam-Template/" target="_blank" rel="noreferrer">
+                                <i className="fa-solid fa-link"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="workItem bg-[url('/assets/images/portfolio/Project__bg-5.png')] w-full relative bg-cover">
+                    <div class="workItem__img w-96 h-96 cursor-pointer transform hover:scale-110 duration-500">
+                    </div>
+                    <div class="workItem__content text-white bg-slate-800 flex justify-between items-center p-5">
+                        <h5>PSD to HTML</h5>
+                        <div className="portfolioItem__info-link w-10 h-10 leading-10 bg-red-600 text-center border-2 border-red-800 rounded-full transform hover:scale-110 duration-300">
+                            <a href="https://positiveworldbd.github.io/Tajam-Template/" target="_blank" rel="noreferrer">
+                                <i className="fa-solid fa-link"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="workItem bg-[url('/assets/images/portfolio/Project__bg-6.png')] w-full relative bg-cover">
+                    <div class="workItem__img w-96 h-96 cursor-pointer transform hover:scale-110 duration-500">
+                    </div>
+                    <div class="workItem__content text-white bg-slate-800 flex justify-between items-center p-5">
+                        <h5>PSD to HTML</h5>
+                        <div className="portfolioItem__info-link w-10 h-10 leading-10 bg-red-600 text-center border-2 border-red-800 rounded-full transform hover:scale-110 duration-300">
+                            <a href="https://positiveworldbd.github.io/Tajam-Template/" target="_blank" rel="noreferrer">
+                                <i className="fa-solid fa-link"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="workItem bg-[url('/assets/images/portfolio/Project__bg-7.png')] w-full relative bg-cover">
+                    <div class="workItem__img w-96 h-96 cursor-pointer transform hover:scale-110 duration-500">
+                    </div>
+                    <div class="workItem__content text-white bg-slate-800 flex justify-between items-center p-5">
+                        <h5>PSD to HTML</h5>
+                        <div className="portfolioItem__info-link w-10 h-10 leading-10 bg-red-600 text-center border-2 border-red-800 rounded-full transform hover:scale-110 duration-300">
+                            <a href="https://positiveworldbd.github.io/Tajam-Template/" target="_blank" rel="noreferrer">
+                                <i className="fa-solid fa-link"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="workItem bg-[url('/assets/images/portfolio/Project__bg-9.png')] w-full relative bg-cover">
                     <div class="workItem__img w-96 h-96 cursor-pointer transform hover:scale-110 duration-500">
                     </div>
                     <div class="workItem__content text-white bg-slate-800 flex justify-between items-center p-5">
